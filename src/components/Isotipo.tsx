a2a8ce271b1ef7969e73a1563e01c3bfedc63a20
Jorge Ballote <ealjkj@gmail.com>
2023-07-17 04:44:33 -0600

    Refactors Main Page

--- src/components/Isotipo.tsx
+++ src/components/Isotipo.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
-import logo from "../../public/aria/brand/logotipo.png";
+import logo from "../../public/aria/brand/isotipo.png";
 
-export default function Logo() {
+export default function Isotipo() {
   return (
-    <a href="#" className="logo-anchor">
+    <a href="#" className="logo-anchor isotipo">
       <Image
         // src="/aria/brand/logotipo.png"
         src={logo}
