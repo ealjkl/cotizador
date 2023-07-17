407a03bd3c8e8224db809045f36788d2c0651b2a
Jorge Ballote <ealjkj@gmail.com>
2023-07-17 07:22:28 -0600

    Makes the modal almost functional

--- src/components/quoter/TotalPriceSection.tsx
+++ src/components/quoter/TotalPriceSection.tsx
@@ -5,7 +5,7 @@ export default function TotalPriceSection() {
   const { pagoTotal } = useQuoterContext();
   return (
     <section className="quoter__price-section">
-      <h2 className="quoter__total-price__label">Precio total:</h2>
+      <h2 className="quoter__total-price__label">Precio total</h2>
       <p className="quoter__total-price__value-container">
         <span className="quoter__total-price__value">
           {moneyFormater.format(pagoTotal)}
