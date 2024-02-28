9e6fe9d9b72132b05ad0ba39fc4e20a6386828fa
Luis Ballote <=>
2024-02-28 13:22:53 -0600

    deleted unused component

--- src/components/quoter/PlazoSection.tsx
+++ src/components/quoter/PlazoSection.tsx
@@ -1,6 +1,4 @@
-import useQuoterContext from "@/hooks/useQuoterContext";
 import PlanSelect from "./inputs/plan/PlanSelect";
-import PlazoSelect from "./inputs/plazo/PlazoSelect";
 
 type Props = {};
 
